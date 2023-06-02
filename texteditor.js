1d242f933afa1bafee9ccd5ba8109659728a7bb1
deepika yadav <84773214+Deepika16008@users.noreply.github.com>
2023-06-02 23:01:19 +0000

    Add files via upload

---
+++ texteditor.js
@@ -0,0 +1,18 @@
+function f1(){
+    document.getElementById("textarea1").style.fontWeight="bold";
+}
+function f2(){
+    document.getElementById("textarea1").style.fontStyle="italic";
+}
+function f3(){
+    document.getElementById("textarea1").style.textAlign="left";
+}
+function f4(){
+    document.getElementById("textarea1").style.textAlign="center";
+}
+function f5(){
+    document.getElementById("textarea1").style.textTransform="UPPERCASE";
+}
+function f6(){
+    document.getElementById("textarea1").style.textTransform="lowercase";
+}
